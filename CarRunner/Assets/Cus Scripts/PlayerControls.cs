56f3d5670b7f15c9b29d12bbbb6785d2fcae1ef8
tcaffelle032 <tcaffelle@gmail.com>
2014-04-29 16:13:23 -0400

    Added base code

---
+++ CarRunner/Assets/Cus Scripts/PlayerControls.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+	Animator playerAnimation;
+	// Use this for initialization
+	void Start () {
+		playerAnimation = this.gameObject.GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+
+			playerAnimation.SetBool("IsJumping",true);
+			playerJump();
+
+		}
+	}
+
+	void playerJump(){
+		var playerPos = this.gameObject.transform;
+		Vector3 jump = new Vector3(0,2,0);
+		playerPos.position += jump;				
+		playerAnimation.SetBool("IsJumping",true);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collider){
+		if(collider.gameObject.name =="Ground"){
+			playerAnimation.SetBool("IsJumping",false);
+			//Debug.Log("False");
+
+		}
+	}
+
+
+}
---
+++ CarRunner/Assets/Cus Scripts/Treadmill.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Treadmill : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject ground = GameObject.Find("Ground");
+
+		ground.transform.position -= new Vector3(.25f,0,0);
+
+		if(ground.transform.position.x <= -ground.transform.transform.localScale.x - ground.transform.position.x){
+			//moreGround(player,ground,ground.transform.position);
+
+				
+		}
+
+	}
+
+	public void moreGround(GameObject player,GameObject treadmill,Vector3 position){
+		var randNum = Random.Range(0,10);
+		var newPiece = GameObject.Instantiate(treadmill,new Vector3(randNum,treadmill.transform.position.y,0),Quaternion.identity);
+
+		newPiece.name = "Ground";
+	}
+	void destroyGround(GameObject ground){
+		GameObject.Destroy(ground);
+	}
+}
